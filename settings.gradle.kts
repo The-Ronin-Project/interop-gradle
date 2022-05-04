424af4606879ef24de842866a194912997beca28
Ed Phillips <ed@projectronin.com>
2022-05-04 08:31:45 -0700

    Upgraded org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin from 10.2.1 to 10.3.0 (#48)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "interop-gradle"
 pluginManagement {
     plugins {
         kotlin("jvm") version "1.6.10"
-        id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+        id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
         id("pl.allegro.tech.build.axion-release") version "1.13.6"
         id("com.dipien.releaseshub.gradle.plugin") version "3.1.0"
     }
